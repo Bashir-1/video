c65e6954c913a5c75406ff8a2e6af2e03261979d
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-10-09 12:50:12 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://video-api.ds07.eu.org/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MmUzYWJjYjcxMjg1YWFhODRiMzRlYWE1ZjI4MjQyMyIsInN1YiI6IjY0NWUyZThlODhiMTQ4MDE1OGYyZDA4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K1cL8aqSY_wSKC4tk-SnL7EFyeMCRv9q_To-vq2lxcw"
+};
